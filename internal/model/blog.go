f825710fe7c7bb38b16056a166d272a2100dd0cd
aifuxi <aifuxi.js@gmail.com>
2025-12-27 21:23:07 +0800

    feat: 博客的 CategoryID 返回字符串

--- internal/model/blog.go
+++ internal/model/blog.go
@@ -13,7 +13,7 @@ type Blog struct {
 	Published   bool       `gorm:"default:false;comment:是否发布" json:"published"`
 	PublishedAt *time.Time `gorm:"comment:发布时间" json:"publishedAt,omitempty"`
 
-	CategoryID int64     `gorm:"comment:分类ID" json:"categoryID"`
+	CategoryID int64     `gorm:"comment:分类ID" json:"categoryID,string"`
 	Category   *Category `gorm:"foreignKey:CategoryID" json:"category,omitempty"`
 
 	Tags []*Tag `gorm:"many2many:blog_tags;" json:"tags"`
