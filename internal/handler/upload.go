75cf636e472be1fe312068877a06adc4a48ca82a
aifuxi <aifuxi.js@gmail.com>
2025-12-28 20:26:30 +0800

    feat: 收敛 upload 逻辑到 service 层

--- internal/handler/upload.go
+++ internal/handler/upload.go
@@ -1,23 +1,18 @@
 package handler
 
 import (
-	"context"
-	"strings"
-	"time"
-
-	"github.com/aifuxi/fgo/config"
 	"github.com/aifuxi/fgo/internal/model/dto"
-	"github.com/aifuxi/fgo/pkg/logger"
+	"github.com/aifuxi/fgo/internal/service"
 	"github.com/aifuxi/fgo/pkg/response"
-	"github.com/aifuxi/fgo/pkg/upload"
-	"github.com/aliyun/alibabacloud-oss-go-sdk-v2/oss"
 	"github.com/gin-gonic/gin"
 )
 
-type UploadHandler struct{}
+type UploadHandler struct {
+	svc service.UploadService
+}
 
-func NewUploadHandler() *UploadHandler {
-	return &UploadHandler{}
+func NewUploadHandler(svc service.UploadService) *UploadHandler {
+	return &UploadHandler{svc: svc}
 }
 
 func (h *UploadHandler) UploadPresign(ctx *gin.Context) {
@@ -28,33 +23,11 @@ func (h *UploadHandler) UploadPresign(ctx *gin.Context) {
 		return
 	}
 
-	filename := config.AppConfig.OSS.UploadDir + "/" + req.Name
-
-	result, err := upload.GetClient().Presign(context.TODO(), &oss.PutObjectRequest{
-		Bucket:      oss.Ptr(config.AppConfig.OSS.Bucket),
-		Key:         oss.Ptr(filename),
-		ContentType: oss.Ptr("application/octet-stream"),
-	},
-		oss.PresignExpires(10*time.Minute),
-	)
+	result, err := h.svc.UploadPresign(ctx, &req)
 	if err != nil {
-		logger.Sugar.Fatalf("failed to put object presign %v", err)
 		response.BusinessError(ctx, err.Error())
 		return
 	}
 
-	if len(result.SignedHeaders) > 0 {
-		//当返回结果包含签名头时，使用签名URL发送Put请求时，需要设置相应的请求头
-		logger.Sugar.Infof("signed headers:\n")
-		for k, v := range result.SignedHeaders {
-			logger.Sugar.Infof("%v: %v\n", k, v)
-		}
-	}
-
-	response.Success(ctx, dto.UploadPresignResp{
-		URL:           strings.SplitN(result.URL, "?", 2)[0],
-		Name:          req.Name,
-		SignedHeaders: result.SignedHeaders,
-		UploadURL:     result.URL,
-	})
+	response.Success(ctx, result)
 }
--- router/routes/upload_routes.go
+++ router/routes/upload_routes.go
@@ -2,11 +2,12 @@ package routes
 
 import (
 	"github.com/aifuxi/fgo/internal/handler"
+	"github.com/aifuxi/fgo/internal/service"
 	"github.com/gin-gonic/gin"
 )
 
 func RegisterUploadRoutes(api *gin.RouterGroup) {
-	h := handler.NewUploadHandler()
+	h := handler.NewUploadHandler(service.NewUploadService())
 
 	authRoutes := api.Group("/upload")
 	{
